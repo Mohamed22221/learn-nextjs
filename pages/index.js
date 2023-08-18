562a73ac385e2959b5e354f6af16013106a709b1
Mohamed Hamdy <99207823+Mohamed22221@users.noreply.github.com>
2023-08-19 01:23:17 +0300

    feat : router.push() & router.replace()

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,16 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function homePage() {
+  const router = useRouter()
+  const navigateBlogs = () =>{
+    router.push("/blogs")
+    // router.push({pathname : "/blogs"})
+    // router.replace("/blogs")
+
+  }
   return (
     <>
       <h1>start home</h1>
@@ -19,6 +27,7 @@ export default function homePage() {
           <Link href="about">About</Link>
         </li>
       </ul>
+      <button onClick={navigateBlogs}>Go Blogs Bage</button>
     </>
   );
 }
