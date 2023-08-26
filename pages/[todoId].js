e1209f543a54c308844357d67fa66a8f8f214c31
Mohamed22221 <hamdymohame7@gmail.com>
2023-08-26 20:31:41 +0300

    get details todo using getStaticPaths

--- pages/[todoId].js
+++ pages/[todoId].js
@@ -1,24 +1,34 @@
-import React from 'react'
+import React from "react";
 
-const TodoDetailsPage = ({todoDatails}) => {
-  return (
-    <div>{todoDatails.title}</div>
-  )
-}
+const TodoDetailsPage = ({ todoDatails }) => {
+  return <div>{todoDatails.title}</div>;
+};
 
-export default TodoDetailsPage
+export default TodoDetailsPage;
 
 export async function getStaticProps(context) {
-    const {params} = context
-    const todoId = params.todoId
-    const response = await fetch("https://jsonplaceholder.typicode.com/todos");
-    const todos = await response.json();
-    const todoDatails = todos.find((item) => item.id === todoId)
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/todos/${context.params.todoId}`
+  );
+  const todoDatails = await response.json();
+  return {
+    props: {
+      todoDatails,
+    },
+    revalidate: 5, // Regenerate the page every 20 seconds
+  };
+}
+
+export async function getStaticPaths() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const todos = await response.json();
+  const todosPaths = todos.map((item) => {
     return {
-      props: {
-        todoDatails,
-      },
-      revalidate: 5, // Regenerate the page every 20 seconds
+      params: { todoId: `${item.id}` },
     };
-  }
-  
+  });
+  return {
+    paths: todosPaths,
+    fallback: false,
+  };
+}
