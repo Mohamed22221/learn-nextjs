62e03cef5b1a5331e72c57dbb365b0127ce4b326
Mohamed22221 <hamdymohame7@gmail.com>
2023-08-30 16:22:19 +0300

    feat : req and res context serverside

--- pages/posts/[idServerPosts].js
+++ pages/posts/[idServerPosts].js
@@ -4,8 +4,10 @@ const TodoDetailsServerPage = ({ postsDatails }) => {
 export default TodoDetailsServerPage;
 
 export async function getServerSideProps(context) {
+  const {params , req ,res} = context
+  console.log(req , "req")
   const response = await fetch(
-    `https://jsonplaceholder.typicode.com/posts/${context.params.idServerPosts}`
+    `https://jsonplaceholder.typicode.com/posts/${params.idServerPosts}`
   );
   const postsDatails = await response.json();
   return {
